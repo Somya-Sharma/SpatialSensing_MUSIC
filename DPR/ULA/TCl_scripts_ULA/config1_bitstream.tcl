1dc31141f171b032f3a4e8f95a876eb267dba066
Somya-Sharma <32894464+Somya-Sharma@users.noreply.github.com>
2021-05-24 19:01:49 +0530

    Delete DPR/ULA directory

--- DPR/ULA/TCl_scripts_ULA/config1_bitstream.tcl
+++
@@ -1,3 +0,0 @@
-write_bitstream -force ./Bitstreams/Config1/config1.bit
-write_cfgmem -force -format BIN -interface SMAPx32 -disablebitswap -loadbit "up 0x0 ./Bitstreams/Config1/config1_pblock_inst_partial.bit" "./Bin_files/config1.bin"
-close_project
